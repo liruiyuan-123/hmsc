fa5241f212e0fd5334f30806a57e782821e12a0a
lry <lry@example.com>
2020-04-22 17:35:37 +0800

    登录模板，js完成，

--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,jsonify
 
 router_user = Blueprint('user_page',__name__)
 
@@ -7,9 +7,30 @@ router_user = Blueprint('user_page',__name__)
 def register():
     return '注册'
 # 登录
-@router_user.route("/login")
+@router_user.route("/login",methods=['GET','POST'])
 def login():
-    return render_template("user/login.html")
+    if request.method == "GET":
+        return render_template("user/login.html")
+    resp = {
+        'code':200,
+        'msg':'登录成功',
+        'data':{}
+    }
+    
+    req = request.values
+    login_name = req['login_name'] if 'login_name' in req else ''
+    login_pwd = req['login_pwd'] if 'login_pwd' in req else ''
+
+    if login_name is None or len(login_name)<1:
+        resp['code'] = -1
+        resp['msg'] = "请输入正确的用户名"
+
+        return jsonify(resp)
+    if login_name is None or len(login_pwd)<1:
+        resp['code'] = -1
+        resp['msg'] = "请输入正确的密码"
+        return jsonify(resp)
+    return jsonify(resp)
 
 @router_user.route("/logout")
 def logout():
--- web/static/js/common.js
+++ web/static/js/common.js
@@ -2,6 +2,14 @@
 var common_ops = {
     init:function(){
         console.log('common.js初始化')
+        this.eventBind()
+    },
+    eventBind:function(){
+        console.log('common.js初始化')
+    },
+    buildUrl:function(path,params){
+        var url = ""+path
+        return url;
     }
 }
 
