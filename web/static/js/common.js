9c8dcb7eb72e3d9808289c28d99075c0e4091bdb
lry <lry@example.com>
2020-04-22 15:18:45 +0800

    解决静态文件无法在蓝图加载的问题

--- web/static/js/common.js
+++ web/static/js/common.js
@@ -2,12 +2,9 @@
 var common_ops = {
     init:function(){
         console.log('common.js初始化')
-    },
-    // buildUrl:function(path,params){
-    //     var url = "" + path
-    //     return url
-    // }
+    }
 }
+
 $(document).ready(function(){
     common_ops.init();
 })
